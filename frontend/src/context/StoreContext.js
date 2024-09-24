e1a49dee89888312bacfb2836ca87d44ab0f8ded
= <bruniv.dev@gmail.com>
2024-09-24 19:58:10 +0530

    process.env.node_env

--- frontend/src/context/StoreContext.js
+++ frontend/src/context/StoreContext.js
@@ -6,7 +6,13 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   // const url = "http://localhost:5000";
-  const url = "https://yumzy-api.onrender.com";
+  // const url = "https://yumzy-api.onrender.com";
+
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000"
+      : "https://yumzy-api.onrender.com";
+
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
@@ -97,89 +103,3 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
-
-//go to index.js and wrap app in StoreContextProvider
-
-// // FOOD DATA FROM ASSESTS.JS
-// //  setting up a React Context, which is a way to share data across different
-// // parts of a React application without passing props manually at every level.
-
-// // importing the createContext function from React to create a Context object.
-// import { createContext, useEffect, useState } from "react";
-// import { food_list } from "../assets/assets";
-
-// // a new Context object named StoreContext is created.
-// //The null you see is the default value of the context
-// // when no provider is found above in the component tree.
-// export const StoreContext = createContext(null);
-
-// const StoreContextProvider = (props) => {
-//   const [cartItems, setCartItems] = useState({});
-//   // This is a React component called StoreContextProvider.
-//   //It wraps its children components with a StoreContext.Provider.
-//   //The value prop of the provider is set to contextValue,
-//   //which is currently an empty object.
-//   //This is where you would put any data or functions you want to share with components
-//   // that consume this context.
-
-//   //foodlist array can be accessed anywhere
-
-//   const url = "http://localhost:5000";
-//   const [token, setToken] = useState("");
-
-//   const addToCart = (itemId) => {
-//     if (!cartItems[itemId]) {
-//       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
-//     } else {
-//       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-//     }
-//   };
-
-//   const removeFromCart = (itemId) => {
-//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-//   };
-
-//   const getTotalCartAmount = () => {
-//     let totalAmount = 0;
-//     for (const item in cartItems) {
-//       if (cartItems[item] > 0) {
-//         let itemInfo = food_list.find((product) => product._id === item);
-//         totalAmount += itemInfo.price * cartItems[item];
-//       }
-//     }
-//     return totalAmount;
-//   };
-
-//   // useEffect(() => {
-//   //   console.log(cartItems);
-//   // }, [cartItems]);
-
-//   // to avoid logout during reload page, set token on reload
-//   useEffect(() => {
-//     if (localStorage.getItem("token")) {
-//       setToken(localStorage.getItem("token"));
-//     }
-//   }, []);
-
-//   const contextValue = {
-//     food_list,
-//     cartItems,
-//     setCartItems,
-//     addToCart,
-//     removeFromCart,
-//     getTotalCartAmount,
-//     url,
-//     token,
-//     setToken,
-//   };
-
-//   return (
-//     <StoreContext.Provider value={contextValue}>
-//       {props.children}
-//     </StoreContext.Provider>
-//   );
-// };
-
-// export default StoreContextProvider;
-
-// //go to index.js and wrap app in StoreContextProvider
