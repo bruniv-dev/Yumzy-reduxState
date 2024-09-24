a70dc4b37f4567d7ad8294abf6e0f5171ad28e4c
= <bruniv.dev@gmail.com>
2024-09-24 19:42:07 +0530

    database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.wjldf.mongodb.net/yumzy`
+      `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.amzfb.mongodb.net/yumzy-reduxState`
     )
     .then(() => {
       console.log("Connected to mongodb database");
